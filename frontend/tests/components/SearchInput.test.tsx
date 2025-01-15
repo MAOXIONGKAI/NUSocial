3d7724cf66897b7adf4d5c8ba7a029ac0dd47ae2
MAOXIONGKAI <conan7153@gmail.com>
2025-01-15 19:07:06 +0800

    Fix frontend CI error due to failing Unit Test and add more test cases

--- frontend/tests/components/SearchInput.test.tsx
+++ frontend/tests/components/SearchInput.test.tsx
@@ -4,7 +4,7 @@ import SearchInput from "../../src/components/SearchInput";
 
 describe('SearchInput', () => {
     it('should render the input text field when shown', () => {
-        render(<SearchInput searchKeyword={""} setSearchKeyword={vi.fn()}/>)
+        render(<SearchInput setSearchKeyword={vi.fn()}/>)
 
         expect(screen.getByRole('textbox'))
             .toHaveAttribute("placeholder", expect.stringMatching(/search post/i))
@@ -12,15 +12,27 @@ describe('SearchInput', () => {
 
     it('should updates its value when user types in it', async () => {
         const setSearchKeywordMock = vi.fn()
-        render(<SearchInput searchKeyword={""} setSearchKeyword={setSearchKeywordMock}/>)
+        render(<SearchInput setSearchKeyword={setSearchKeywordMock}/>)
 
         const user = userEvent.setup()
         const input = screen.getByRole('textbox')
         const message = "Hello world"
         await user.type(input, message)
 
-        expect(setSearchKeywordMock).toHaveBeenCalledTimes(message.length)
-        message.split('').forEach((char) =>
-            expect(setSearchKeywordMock).toHaveBeenCalledWith(char))
+        expect(input).toHaveValue(message)
+    })
+
+    it('should updates the search keyword properly when enter key is pressed', async () => {
+        const setSearchKeywordMock = vi.fn()
+        render(<SearchInput setSearchKeyword={setSearchKeywordMock}/>)
+
+        const user = userEvent.setup()
+        const input = screen.getByRole('textbox')
+        const message = "Goodbye World"
+        await user.type(input, message)
+        await user.keyboard('{Enter}')
+
+        expect(setSearchKeywordMock).toHaveBeenCalledTimes(1)
+        expect(setSearchKeywordMock).toHaveBeenCalledWith(message)
     })
 })
