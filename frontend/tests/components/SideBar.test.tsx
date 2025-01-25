6ec28caf9325bfee2e24c0be486ae1c0186f1fcc
MAOXIONGKAI <conan7153@gmail.com>
2025-01-25 20:09:14 +0800

    Fix frontend CI error due to failing test cases

--- frontend/tests/components/SideBar.test.tsx
+++ frontend/tests/components/SideBar.test.tsx
@@ -1,28 +1,22 @@
 import {render, screen} from "@testing-library/react";
 import SideBar from "../../src/components/SideBar";
-import {sectionMenuOptions, categoryMenuOptions} from "../../src/data/MenuData";
+import {categoryMenuOptions} from "../../src/data/MenuData";
 import {expect} from "vitest";
 
 const propsMock = {
-    setSectionCondition: vi.fn(),
     setCategoryCondition: vi.fn(),
 }
 
 describe('SideBar', () => {
     it('should render all the section and category options properly', () => {
-        render(<SideBar {...propsMock} />)
+        render(<SideBar categoryCondition="" {...propsMock} />)
 
         // Testing presence of option text
-        sectionMenuOptions.map(option => {
-            expect(screen.getByText(option.text)).toBeInTheDocument()
-        })
         categoryMenuOptions.map(option => {
             expect(screen.getByText(option.text)).toBeInTheDocument()
         })
 
         // Testing presence of Icons
-        expect(screen.getByTestId("HomeIcon")).toBeInTheDocument() // Home
-        expect(screen.getByTestId("FavoriteIcon")).toBeInTheDocument() // Favorite
         expect(screen.getByTestId("MenuBookIcon")).toBeInTheDocument() // Study
         expect(screen.getByTestId("Diversity1Icon")).toBeInTheDocument() // CCA
         expect(screen.getByTestId("LocationCityIcon")).toBeInTheDocument() // Campus
