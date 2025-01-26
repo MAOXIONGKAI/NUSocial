bd51c4852caf429fd309c0aec010df2d01b1e11a
MAOXIONGKAI <conan7153@gmail.com>
2025-01-26 11:27:25 +0800

    Implement home option for sidebar

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -2,14 +2,27 @@ import logo from '../assets/logo.png'
 
 type Props = {
     style?: React.CSSProperties
+    setCategoryCondition: (categoryCondition: string) => void
 }
 
-export default function Logo({style}: Props) {
+export default function Logo({style, setCategoryCondition}: Props) {
     return (
-        <img
-            src={logo}
-            alt="NUSocial Logo"
-            {...(style && {style})}
-        />
+        <button
+            style={{
+                border: "none",
+                background: "none",
+                padding: 0,
+                cursor: "pointer",
+                width: "75px",
+                margin: "0% 7% 0% 3%"
+            }}
+            onClick={() => setCategoryCondition("")}
+        >
+            <img
+                src={logo}
+                alt="NUSocial Logo"
+                {...(style && {style})}
+            />
+        </button>
     )
 }
