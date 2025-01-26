bb6ee7006a7b9772afc071d65ad09188c6346c92
MAOXIONGKAI <conan7153@gmail.com>
2025-01-26 13:17:01 +0800

    Change the theme of the website to blue

--- frontend/src/data/Config.ts
+++ frontend/src/data/Config.ts
@@ -1,1 +1,24 @@
+import {createTheme} from "@mui/material";
+
 export const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+export const appTheme = createTheme({
+    components: {
+        MuiSvgIcon: {
+            styleOverrides: {
+                root: {
+                    color: '#1976d2',
+                },
+            },
+        },
+        MuiAvatar: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: 'white',
+                    border: '2px solid #1976d2',
+                    borderRadius: "50%"
+                },
+            },
+        },
+    },
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import {UserProvider} from './context-providers/UserProvider.tsx'
 import {SnackBarProvider} from "./context-providers/SnackBarProvider.tsx";
+import {ThemeProvider} from "@mui/material";
+import {appTheme} from "./data/Config.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <SnackBarProvider>
-            <UserProvider>
-                <App/>
-            </UserProvider>
-        </SnackBarProvider>
-    </StrictMode>,
+        <ThemeProvider theme={appTheme}>
+            <SnackBarProvider>
+                <UserProvider>
+                    <App/>
+                </UserProvider>
+            </SnackBarProvider>
+        </ThemeProvider>
+    </StrictMode>
 )
